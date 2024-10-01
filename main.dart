d1a483a32da4fcd088523536edb65ee6f642f338
meshariSalhouli <mesharialhouli12@gmail.com>
2024-10-01 08:43:25 +0300

    dart1

---
+++ main.dart
@@ -0,0 +1,57 @@
+void main() {
+
+int mark = 80;
+int i = 3;
+
+
+
+if( mark >= 80 ){
+
+  print("A");
+
+  
+}
+  else if (70 >= mark && mark < 80){
+
+print("B");
+  }
+else if (60>= mark && mark < 70){
+
+print("C");
+  }
+
+else if (60 >= mark || mark < 50){
+
+print("D");
+  }
+
+else  () {
+
+print("F");
+ 
+ 
+  };
+
+//Practice: FizzBuzz
+  if( i%3==0 && i%5==0 ){
+
+  print("fizz buzz");
+
+  
+}
+  else if (i%3==0){
+
+print("fizz");
+
+}
+
+
+  else if (i%3==0){
+
+print("buzz");
+
+}
+else(){
+print("1");
+};
+}
